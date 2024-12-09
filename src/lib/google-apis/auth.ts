b20979f9acab1bca302791e43710eab957b793cd
imrany <imranmat254@gmail.com>
2024-12-09 17:05:54 +0300

    hide the keyFile path to env

--- src/lib/google-apis/auth.ts
+++ src/lib/google-apis/auth.ts
@@ -1,15 +1,7 @@
 import { google } from "googleapis";
-import path from "path"
-// import fs from "fs"
-import { fileURLToPath } from 'url'
-    
-const __filename = fileURLToPath(import.meta.url)
 
-const __dirnameNew = path.dirname(__filename)
-
-const keyFile = path.join(__dirnameNew,"key/villebiz-7d68d9a6ebb9.json")
 const client = new google.auth.GoogleAuth({ 
-    keyFile, 
+    keyFile:`${process.env.KEY_FILE}`, 
     scopes: [ 'https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive' ], 
 }); 
 
